81e5f78ebc4cde0064e22ca4748c997d347c5b2f
savarbhasin <savarbhasin446@gmail.com>
2024-01-23 21:54:41 +0530

    add hackathons page, content collection and responsive hero,navbar section

---
+++ src/content/config.ts
@@ -0,0 +1,40 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const eventCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        name: z.string(),
+        date: z.coerce.date(),
+        image: z.string().optional(),
+        link: z.string().optional(),
+        description:z.string().optional(),
+    }),
+});
+
+const hackathonCollection = defineCollection({
+    type:'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.coerce.date(),
+        image: z.string().optional(),
+        description: z.string().optional(),
+        judges: z.array(z.string()).optional(),
+        link: z.string().optional()
+    })
+})
+const projectCollection = defineCollection({
+    type:'content',
+    schema: z.object({
+        title:z.string(),
+        contributors: z.array(z.string()),
+        technlogies: z.array(z.string()),
+        status: z.string()
+    })
+})
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'events': eventCollection,
+  'hackathons':hackathonCollection
+};
---
+++ src/content/hackathons/hackathon-1.md
@@ -0,0 +1,7 @@
+---
+title: Education, Binge Watching, Anything New, ML for X
+date: 2018-03-28
+judges: [Lohit, Palash Bansal, Kushagra Singh, Varun Bansal]
+link: https://www.facebook.com/events/164134847737373/
+
+---
