0fb3e3d2c03ad4627c6794c5e7307eefcdae0308
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2024-02-09 17:51:34 +0530

    github pages test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://anirudh-s-kumar.github.io/",
+  base: "/byld-website-new",
   integrations: [
     react(),
     tailwind({
